f3b4f6d2153743a555bf806a7be812634e51dfb1
minkj1992 <minkj1992@gmail.com>
2021-03-19 19:52:17 +0900

    feat: hello world react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div></div>;
-}
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+const App: React.FC = () => {
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route path="/" exact>
+                        <h1>home</h1>
+                    </Route>
+                    <Route path="/hello">
+                        <h1>hello world</h1>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
